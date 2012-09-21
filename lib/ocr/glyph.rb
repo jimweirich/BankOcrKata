f967423de4a57605295ae190d8e1fe6bf2a448a5
Jim Weirich <jim.weirich@gmail.com>
2012-09-21 05:27:42 +0300

    move glyph conversion hashes to their own module

--- lib/ocr/glyph.rb
+++ lib/ocr/glyph.rb
@@ -1,58 +1,10 @@
+require 'ocr/glyph_conversions'
+
 module OCR
   class IllformedGlyphError < StandardError; end
 
   class Glyph
-    TO_DIGIT = {
-      " _ " +
-      "| |" +
-      "|_|" => "0",
-
-      "   " +
-      "  |" +
-      "  |" => "1",
-
-      " _ " +
-      " _|" +
-      "|_ " => "2",
-
-      " _ " +
-      " _|" +
-      " _|" => "3",
-
-      "   " +
-      "|_|" +
-      "  |" => "4",
-
-      " _ " +
-      "|_ " +
-      " _|" => "5",
-
-      " _ " +
-      "|_ " +
-      "|_|" => "6",
-
-      " _ " +
-      "  |" +
-      "  |" => "7",
-
-      " _ " +
-      "|_|" +
-      "|_|" => "8",
-
-      " _ " +
-      "|_|" +
-      " _|" => "9",
-    }
-
-    def self.by_width(string)
-      string.scan(/.../)
-    end
-
-    def self.reverse_lookup(to_digit)
-      to_digit.inject({}) { |h, (k,v)| h.merge(v => by_width(k)) }
-    end
-
-    FROM_DIGIT = reverse_lookup(TO_DIGIT)
+    include GlyphConversions
 
     attr_reader :value
 
@@ -101,7 +53,7 @@ module OCR
     end
 
     def by_width(string)
-      self.class.by_width(string)
+      GlyphConversions.by_width(string)
     end
 
     def normalize_line_lengths
---
+++ lib/ocr/glyph_conversions.rb
@@ -0,0 +1,55 @@
+module OCR
+  module GlyphConversions
+    TO_DIGIT = {
+      " _ " +
+      "| |" +
+      "|_|" => "0",
+
+      "   " +
+      "  |" +
+      "  |" => "1",
+
+      " _ " +
+      " _|" +
+      "|_ " => "2",
+
+      " _ " +
+      " _|" +
+      " _|" => "3",
+
+      "   " +
+      "|_|" +
+      "  |" => "4",
+
+      " _ " +
+      "|_ " +
+      " _|" => "5",
+
+      " _ " +
+      "|_ " +
+      "|_|" => "6",
+
+      " _ " +
+      "  |" +
+      "  |" => "7",
+
+      " _ " +
+      "|_|" +
+      "|_|" => "8",
+
+      " _ " +
+      "|_|" +
+      " _|" => "9",
+    }
+
+    def self.by_width(string)
+      string.scan(/.../)
+    end
+
+    def self.reverse_lookup(to_digit)
+      to_digit.inject({}) { |h, (k,v)| h.merge(v => by_width(k)) }
+    end
+
+    FROM_DIGIT = reverse_lookup(TO_DIGIT)
+  end
+end
