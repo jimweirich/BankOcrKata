1d46e9921c24088ec881ddc39405ed4f83b31fb9
Jim Weirich <jim.weirich@gmail.com>
2012-09-23 17:13:15 -0500

    add compliance level to user stories

--- stories/story_spec.rb
+++ stories/story_spec.rb
@@ -5,9 +5,11 @@ describe "User Stories" do
     open(file_name) { |f| f.read }
   end
 
+  Given(:compliance_level) { 0 }
+
   Given(:output_directory) { "tmp" }
-  Given(:input_file_name) { "stories/usecase#{story}.in" }
-  Given(:answer_file_name) { "stories/usecase#{story}.ans" }
+  Given(:input_file_name)  { "stories/usecase#{story}.in" }
+  Given(:answer_file_name) { "stories/usecase#{story}_#{compliance_level}.ans" }
   Given(:output_file_name) { "#{output_directory}/usecase#{story}.out" }
 
   Given { FileUtils.mkdir_p output_directory rescue nil }
@@ -18,19 +20,19 @@ describe "User Stories" do
   Invariant { contents_of(output_file_name).should == contents_of(answer_file_name) }
 
   context "#1" do
-    Given { pending "Completion of User Story 1" }
+    Given { pending "Completion of User Story 1" if compliance_level < 1 }
     Given(:story) { 1 }
     Then { }
   end
 
   context "#3" do
-    Given { pending "Completion of User Story 3" }
+    Given { pending "Completion of User Story 3" if compliance_level < 3 }
     Given(:story) { 3 }
     Then { }
   end
 
   context "#4" do
-    Given { pending "Completion of User Story 4" }
+    Given { pending "Completion of User Story 4" if compliance_level < 4 }
     Given(:story) { 4 }
     Then { }
   end
